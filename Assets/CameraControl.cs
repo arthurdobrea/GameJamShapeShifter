a3a043dc99494a24d5b057207c0291479a282346
arthurdobrea <arthurdobrya@gmail.com>
2020-03-15 12:24:03 +0200

    init

---
+++ Assets/CameraControl.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    public Vector3 offset;
+    public GameObject playerToFollow;
+    public float smoothSpeed = 1;
+    public float speed;
+    private Rigidbody rigidbody;
+
+    public void Start()
+    {
+        rigidbody = playerToFollow.GetComponent<Rigidbody>();
+    }
+
+    public void FixedUpdate()
+    {
+        Vector3 desiredPoistion = playerToFollow.transform.position + offset;
+        Vector3 smoothedPostion = Vector3.Lerp(transform.position, desiredPoistion, smoothSpeed);
+        transform.position = smoothedPostion;
+
+        transform.LookAt(playerToFollow.transform);
+    }
+}
