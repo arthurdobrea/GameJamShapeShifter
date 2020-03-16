35995aa444c61363ba19e33df33bcc9815cf07a1
arthurdobrea <arthurdobrya@gmail.com>
2020-03-16 22:29:51 +0200

    adding lvl

---
+++ Assets/Door.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+
+    public GameObject key;
+
+    private bool isOpened = false;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector3.Distance(transform.position, key.transform.position) < 2)
+        {
+            if (Input.GetKeyDown(KeyCode.F))
+            {
+                if (key.name == "Key")
+                {
+                    transform.Rotate(new Vector3(0,-85,0));
+                }
+            }
+        }
+
+    }
+}
