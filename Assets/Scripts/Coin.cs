4ada2aa5753b99f407de4d2b0369e7ab78aa49cb
Vladislav Gavrisciuc <angelbeats9632@gmail.com>
2020-03-17 13:58:45 +0200

    Coin effect upgrade

--- Assets/Scripts/Coin.cs
+++ Assets/Scripts/Coin.cs
@@ -6,20 +6,29 @@ public class Coin : MonoBehaviour
 {
     public GameManager gameManager;
     public GameObject vfx;
+    private bool collected;
+    private Vector3 scaleFactor = new Vector3(10, 10, 10);
 
     void FixedUpdate()
     {
+        if (collected)
+        {
+            if (transform.localScale.y > 0)
+            {
+                transform.localScale -= scaleFactor * Time.deltaTime;
+            }
+            
+        }
         transform.Rotate(new Vector3(0, 200.0f, 0) * Time.deltaTime, Space.World);
     }
 
     public void Collect()
     {
-        vfx.SetActive(true);
+        collected = true;
         gameManager.coinNumber--;
         Debug.Log(gameManager.coinNumber);
-        vfx.SetActive(true);
-        vfx.transform.SetPositionAndRotation(transform.position, Quaternion.identity);
-        vfx.GetComponent<Vfx>().StartCoroutine("VfxLifeTime");
-        Destroy(gameObject);
+        GameObject tempVfx = Instantiate(vfx, transform.position, Quaternion.identity) as GameObject;
+        Destroy(tempVfx, 1);
+        Destroy(gameObject, 0.1f);
     }
 }
