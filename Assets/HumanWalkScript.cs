d41d00a95da0a4c9bbfa2f281481919234239487
arthurdobrea <arthurdobrya@gmail.com>
2020-03-30 12:51:50 +0300

    Adding FOV Script, not working right now.

---
+++ Assets/HumanWalkScript.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using Random = UnityEngine.Random;
+
+public class HumanWalkScript : MonoBehaviour
+{
+    public GameObject[] pointOfInterest;
+    public NavMeshAgent agent;
+
+    public GameObject currentLocationToMove;
+
+    private void Start()
+    {
+        Move();
+    }
+
+    void FixedUpdate()
+    {
+        if (Vector3.Distance(transform.position, currentLocationToMove.transform.position) <= 1)
+        {
+            Move();
+        }
+    }
+
+    private void Move()
+    {
+        int range = Random.Range(1, 5);
+        currentLocationToMove = pointOfInterest[range];
+        agent.SetDestination(currentLocationToMove.transform.position);
+    }
+}
