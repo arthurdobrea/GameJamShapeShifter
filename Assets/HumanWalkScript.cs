b6a05cf73b316120866086a1453996a53256eeb4
arthurdobrea <arthurdobrya@gmail.com>
2020-03-31 17:28:08 +0300

    Now AI can put player on table if finds it on floor(but sometimes its drops under the floor)

--- Assets/HumanWalkScript.cs
+++ Assets/HumanWalkScript.cs
@@ -8,32 +8,57 @@ using Random = UnityEngine.Random;
 public class HumanWalkScript : MonoBehaviour
 {
     public GameObject[] pointOfInterest;
+    public GameObject placeWhereItPutsPlayer;
     public NavMeshAgent agent;
     public GameObject currentLocationToMove;
 
     private void Start()
     {
-       findNewPlaceToGo();
-       moveToPlace();
+        findNewPlaceToGo();
+        moveToPlace();
     }
 
     void FixedUpdate()
     {
-        if (FovDetection.inFOV(transform, ShapeShifterScript.player.transform, 75, 3))
+        if (FovDetection.inFOV(transform, ShapeShifterScript.player.transform, 75, 3) && GrabAndPlacePlayer.canGrab == false
+                                                                                      && (CharacterController.isMoving || ShapeShifterScript.player.transform.position.y > -0.4 ))
         {
+            Debug.Log("Moving to Player");
+            
             moveToPlayer();
+
+            if (Vector3.Distance(transform.position, ShapeShifterScript.player.transform.position) < 1)
+            {
+                Debug.Log("Goint to grab Player");
+                
+                GrabAndPlacePlayer.canGrab = true;
+            }
+        }
+        else if (GrabAndPlacePlayer.canGrab)
+        {
+            Debug.Log("Going to place player on table");
+            
+            movePlayerToTable();
+
+            if (Vector3.Distance(transform.position, placeWhereItPutsPlayer.transform.position) < 1)
+            {
+                GrabAndPlacePlayer.canGrab = false;
+                GrabAndPlacePlayer.canPut = true;
+            }
         }
         else
         {
+            Debug.Log("GoingToNewPlace");
+            
             moveToPlace();
         }
-
-        if (Vector3.Distance(transform.position, currentLocationToMove.transform.position) <= 1)
+        if (Vector3.Distance(transform.position, currentLocationToMove.transform.position) <= 1 && GrabAndPlacePlayer.canGrab == false)
         {
+            Debug.Log("Finding New PLace To Go");
+            
             findNewPlaceToGo();
             moveToPlace();
         }
-
     }
 
     private void moveToPlace()
@@ -50,5 +75,10 @@ public class HumanWalkScript : MonoBehaviour
     {
         int range = Random.Range(1, 5);
         currentLocationToMove = pointOfInterest[range];
+    }
+
+    private void movePlayerToTable()
+    {
+        agent.SetDestination(placeWhereItPutsPlayer.transform.position);
     }
 }
